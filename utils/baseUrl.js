f5c7801bbf72e26a88d7f9380d17dd6f8d2dee97
mayqf <mayqf@mail.missouri.edu>
2020-09-12 20:10:46 +0200

    new changess

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://.....com/"
+    ? "https://todo-app-pi-blue.vercel.app/"
     : "http://localhost:3000";
 
 export default baseUrl;
