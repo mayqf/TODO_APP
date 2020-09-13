dfc102ceab08c9360ea25127bc1557177969e341
mayqf <mayqf@mail.missouri.edu>
2020-09-13 16:41:29 +0200

    last vers

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://vast-island-73130.herokuapp.com/"
+    ? "https://todo-app-new-one.vercel.app"
     : "http://localhost:3000";
 
 export default baseUrl;
